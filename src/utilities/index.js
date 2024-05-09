65a9846fe7e2dbafa3992f95c2956750b3e34d04
Eleonora Atanasova <eleonora.atanasova.2001@gmail.com>
2024-05-09 17:57:57 +0300

    build: deploy dist for github pages

--- src/utilities/index.js
+++
@@ -1,5 +0,0 @@
-export * from './misc';
-export * from './formatters';
-export { default as memoization } from './Memoization';
-export { default as notice } from './Notice';
-export { updateNavigation } from './updateNavigation';
